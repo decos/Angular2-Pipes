2877f2db2d2de6b19cbf022ae64f27a648ec0c4c
Diego Abanto <diego.abanto@ec.pe>
2017-08-18 02:26:10 -0500

    pipe: date-fecha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,10 +20,12 @@ export class AppComponent {
       calle: "Primera",
       casa: "19"
     }
-  }
+  };
 
   valorDePromesa = new Promise( (resolve,  reject) => {
     setTimeout( () => resolve('Llego la data'),3500 );
-  })
+  });
+
+  fecha = new Date();
 
 }
