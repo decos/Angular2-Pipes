a460b37c0ba1274311240dacaf006404941abac4
Diego Abanto <diego.abanto@ec.pe>
2017-08-18 02:08:00 -0500

    pipe json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,15 @@ export class AppComponent {
   PI = Math.PI;
   a:number = 0.234;
   salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
+
 }
